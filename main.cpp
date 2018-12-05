8314b6ac87cb58bbca11e799232a500c895a45af
Tilo-K <tiloklarenbeek@gmx.de>
2018-12-06 00:13:13 +0100

    Fixed Error

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argn, char** argv)
 {
     int testArray[] = {1,3,4,5,6,9,10,50,90,102}; // has to be sorted
-    int pos = binarySearch(90, testArray ,0 , len(testArray));
+    int pos = binarySearch(90, testArray ,0 , sizeof(testArray));
 
     cout << "Found at " << pos << endl;
 
